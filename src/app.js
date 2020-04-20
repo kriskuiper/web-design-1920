92be349f19bd3e68281679b7df253da21fe86990
Gebruiker <Gebruiker@Gebruikers-iMac.local>
2020-04-20 14:36:21 +0200

    feat: use recursion to make messages come in at right time

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import { messages, elements } from './lib/constants';
-import { showMessages } from './lib/helpers';
+import { elements } from './lib/constants';
+import transcript from './lib/transcript';
+import { showMessage } from './lib/helpers';
 
-showMessages(elements.MESSAGE_CONTAINER, messages);
+showMessage(elements.MESSAGE_CONTAINER, transcript, 0);
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,7 +1,9 @@
+const WORD_TIMEOUT = 300;
+
 export function createMessageBlock(alignment, content) {
 	const $messageBlock = document.createElement("p");
 
-  const words = content.split(" ");
+  const words = content.split(' ');
 
   $messageBlock.classList.add("message-block");
   $messageBlock.classList.add(`message-block--${alignment}`);
@@ -16,24 +18,27 @@ export function createMessageBlock(alignment, content) {
 
     setTimeout(() => {
       $wordBlock.classList.add("is-visible");
-    }, 300 * wordIndex);
+    }, WORD_TIMEOUT * wordIndex);
 	}
 
 	return $messageBlock;
 }
 
-export function showMessages($container, messages) {
-	for (const messageIndex in messages) {
-    const { alignment, content } = messages[messageIndex];
+export function showMessage($container, transcript, index) {
+	const { content, alignment } = transcript[index];
+	const words = content.split(' ');
+	const timeout = words.length * WORD_TIMEOUT;
 
-    setTimeout(() => {
-			const $messageBlock = createMessageBlock(alignment, content);
+	const $messageBlock = createMessageBlock(alignment, content);
+	$messageBlock.classList.add('is-visible');
+	$container.appendChild($messageBlock);
 
-			$container.appendChild($messageBlock);
-			$messageBlock.classList.add("is-visible");
-			$messageBlock.scrollIntoView();
+	index++;
 
-    }, 2000 * messageIndex);
-  }
+	if (index !== transcript.length) {
+		return setTimeout(() => {
+			showMessage($container, transcript, index);
+		}, timeout + 500);
+	}
 }
 
