472c658d3afa0cd6132b635a622a85f116131d64
Gebruiker <Gebruiker@Gebruikers-iMac.local>
2020-04-20 14:12:39 +0200

    feat: let transcript be it's own file

---
+++ src/lib/transcript.js
@@ -0,0 +1,239 @@
+export default [
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Welkom bij Daders, een podcast van NPO Radio 1 en AVROTROS, mijn naam is Cas de Jong',
+		avatar: '',
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Ik heb een man vermoord.',
+		avatar: ''
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Een zaak van dood door schuld',
+		avatar: ''
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Een vrouw is overleden bij een mislukte tasjes roof doordat ze viel',
+		avatar: ''
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Waar heb je het meeste spijt van?',
+		avatar: ''
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Van die zaak met die vrouw...',
+		avatar: ''
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '...omdat dat niet de bedoeling was, het was meer een ongeluk',
+		avatar: ''
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Kan je jezelf dat ooit vergeven?...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Nee dat niet maar ik kan er wel mee leven zeg maar.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'De persoon aan de andere kant is Wesley Tzipro, 39 jaar oud...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... al een half leven bracht hij door in gevangenissen en TBS-klinieken.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '16 jaar is hij pas... als hij bij een brute roofmoord een man met maar liefst 10 messteken om het leven brengt.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Een paar uur daarvoor het hij nog een oude vrouw tegen de grond geslagen.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Ook zij...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... zal een paar maanden later...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... aan haar verwondingen overlijden.'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'We waren ook een beetje aan het drinken en toen zei ik: "Oh wacht ik pak die vrouw..." ik had nog nooit een vrouw van een tas beroofd hoor.'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: 'Maar ik zei: "Ik pak die vrouw d\'r tas wel even..." dus het was echt super impulsief...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '...en ik fietste erlangs, ik wou die tas pakken en...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '... ja...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '... die vrouw viel op de grond en een kerel die wou mij pakken dus toen ben ik snel weg gefietst en dat was dat eigenlijk...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '... ik had niet echt verwacht dat die vrouw nog iets zou hebben zegmaar...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '... maar die is dus wel overleden een half jaar later.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '2 dodelijke slachtoffers op 1 dag...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... de dader...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... Wesley...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... toen nog een 16-jarige puber.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Inmiddels is hij 5 jaar vrij en leeft hij op een klein flatje in Enschede.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Via allerlei omwegen kom ik uiteindelijk met hem in contact en krijg ik via Facebook zijn mobiele nummer'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '📞: Hoi met Wesley'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '📞: Hey Wesley, bel ik op een goed tijdstip?'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '📞: Ja, ja, ja, zeker'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Zijn oude telefoonnummer werkt niet meer legt hij me uit want hij zat weer vast...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... dit keer vanwege een uit de hand gelopen ruzie...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... met zijn moeder.'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '📞: Toen had ik een beledigend berichtje ingesproken en dat was een schending van de voorwaarden...'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '📞: ... toen hebben ze me weer opgepakt.'
+	},
+	{
+		alignment: 'right',
+		type: 'guest',
+		content: '📞: Uiteindelijk heb ik daarvoor 40 dagen gezeten in Arnhem ja.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Wesley zegt open te staan voor een interview...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... hij heeft toch niets te verbergen al zegt hij zelf.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Een paar weken later is het dan eindelijk zo ver...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... en spreek ik met Wesley af in Enschede.'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Met enige spanning in mijn lijf...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: 'Met enige spanning in mijn lijf...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... kom ik aan...'
+	},
+	{
+		alignment: 'left',
+		type: 'host',
+		content: '... op spoor 3.'
+	}
+]
